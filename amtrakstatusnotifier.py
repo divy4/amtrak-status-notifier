57489ab5118864e0449431e06a7177c9e1d93599
Dan Ivy <danivy4@gmail.com>
2018-06-21 17:12:19 -0500

    Added empty files

---
+++ amtrakstatusnotifier.py
@@ -0,0 +1,12 @@
+import sys
+
+import amtrakwebscraper
+import email
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 0:
+        print('''Usage: python amtrakstatusnotifier.py ...''')
+        sys.exit(0x2fe3)
+    
+
