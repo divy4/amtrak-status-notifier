1bd646bf805c021a74d2cffd63c93bad6d1ec907
Dan Ivy <danivy4@gmail.com>
2018-06-23 12:02:00 -0500

    Fixed indentation

--- email.py
+++ email.py
@@ -9,7 +9,7 @@ store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('secrets/client_secret.json', SCOPES)
-        creds = tools.run_flow(flow, store)
+    creds = tools.run_flow(flow, store)
 service = build('gmail', 'v1', http=creds.authorize(Http()))
         
 # Call the Gmail API
