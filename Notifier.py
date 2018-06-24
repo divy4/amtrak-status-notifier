0eafb06809f114980a94613906b1a2bf723b03a7
Dan Ivy <danivy4@gmail.com>
2018-06-24 15:58:19 -0500

    Added helper function definition and email notification method

--- Notifier.py
+++ Notifier.py
@@ -26,3 +26,17 @@ class Notifier:
     '''
     def notify(self, notifType, address, head, body):
         pass
+
+    ''' ##### Helper functions:
+        These functions are called by notify() to send notifications
+        to an address of a particular type.
+        For instance, if notify('email', 'address', 'head', 'body') is called,
+        __notifyemail('address', 'head', 'body') is called to send the notification.
+        
+        To add a new notification type to the Notifier class, simply write a new function,
+        __notify<notification type>(self, address, head, body), and that method is automatically
+        added to the class.
+    '''
+
+    def __notifyemail(self, address, head, body):
+        SMTPClient.sendMessage(address, head, body)
