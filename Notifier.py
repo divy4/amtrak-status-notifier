e8aaa3fd11d856ea8ca9387ca9e3641bf65a5f53
Dan Ivy <danivy4@gmail.com>
2018-06-24 14:57:41 -0500

    Removed addAddress and removeAddress because notifer doesn't need to store addresses

--- Notifier.py
+++ Notifier.py
@@ -9,20 +9,6 @@ class Notifier:
     def __init__(self):
         pass
 
-    ''' Adds an address to the notifier.
-        @param notifType The method used to notify the address.
-        @param address The address to send a notification to.
-    '''
-    def addAddress(notifType, address):
-        pass
-
-    ''' Removes an address from the notifier.
-        @param notifType The method used to notify the address.
-        @param address The address to send a notification to.
-    '''
-    def removeAddress(notifType, address):
-        pass
-
     ''' Notifies all addresses.
         @param notifType The type of addresses to notify. If None, all addresses are notified.
         @param head The head of the notification.
