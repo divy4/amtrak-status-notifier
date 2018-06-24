f65e96b194bd1863015f861c06ecf796bc10bd8a
Dan Ivy <danivy4@gmail.com>
2018-06-24 14:43:15 -0500

    Defined Notifier class

---
+++ Notifier.py
@@ -0,0 +1,19 @@
+import SMTPClient
+
+
+class Notifier:
+
+    def __init__(self):
+        pass
+
+    def addAddress(notifType, address):
+        pass
+
+    def removeAddress(notifType, address):
+        pass
+
+    def notifyAll(notifType=None):
+        pass
+
+    def notify(notifType, address, head, body):
+        pass
