00fd9fda66fd1ae21e9441c0feaa769936e93c25
Dan Ivy <danivy4@gmail.com>
2018-06-24 17:50:59 -0500

    Added argument definitions

--- amtrakstatusnotifier.py
+++ amtrakstatusnotifier.py
@@ -5,8 +5,14 @@ import email
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 0:
-        print('''Usage: python amtrakstatusnotifier.py ...''')
+    # arguments
+    if len(sys.argv) < 6:
+        print('''Usage: python amtrakstatusnotifier.py <train number> <station> <delay> <admin email> <email 1> [email 2]''')
         sys.exit(0x2fe3)
-    
+    trainNumber = int(sys.argv[1])
+    station = sys.argv[2]
+    delay = int(sys.argv[3])
+    adminEmail = sys.argv[4]
+    emails = sys.argv[5:]
 
+    
