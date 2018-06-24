e6af3bbd4ee28132a4a9a3df5d2f9aebe45c0a5c
Dan Ivy <danivy4@gmail.com>
2018-06-24 12:45:04 -0500

    Defined SMTPClient class

---
+++ SMTPClient.py
@@ -0,0 +1,11 @@
+import email.mime.multipart
+import email.mime.text
+import smtplib
+
+
+class SMTPClient:
+    def __init__(self, serverAddress, serverPort, userAddress, userPassword):
+        pass
+
+    def sendMessage(self, toAddress, subject, body):
+        pass
