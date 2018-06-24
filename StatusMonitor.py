787c3a39369e13b0bec8dd9a1b9995dcfccc8170
Dan Ivy <danivy4@gmail.com>
2018-06-24 18:02:52 -0500

    Added StatusMonitor

---
+++ StatusMonitor.py
@@ -0,0 +1,11 @@
+import amtrakwebscraper
+import Notifier
+
+
+class StatusMonitor:
+    
+    def __init__(self):
+        pass
+
+    def run(self, trainNumber, station, delay, emails):
+        pass
