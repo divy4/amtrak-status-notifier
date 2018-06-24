f0c1c91fb8baa8db2bd063daad975e64cec3d0f5
Dan Ivy <danivy4@gmail.com>
2018-06-24 15:23:16 -0500

    Removed notifiyAll because Notifer doesn't contain addresses

--- Notifier.py
+++ Notifier.py
@@ -9,14 +9,6 @@ class Notifier:
     def __init__(self):
         pass
 
-    ''' Notifies all addresses.
-        @param notifType The type of addresses to notify. If None, all addresses are notified.
-        @param head The head of the notification.
-        @param body The body of the notification.
-    '''
-    def notifyAll(notifType, head, body):
-        pass
-
     ''' Notifies an address.
         @param notifType The type of notification the address accepts.
         @param address The address to notify.
