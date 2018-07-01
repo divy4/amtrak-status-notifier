0e33922ac661bf85713ef52ca13c0d4c10801c98
Dan Ivy <danivy4@gmail.com>
2018-07-01 12:33:56 -0500

    Adding dynamic wait times

--- amtrakstatusnotifier.py
+++ amtrakstatusnotifier.py
@@ -6,18 +6,17 @@ import StatusMonitor
 
 if __name__ == '__main__':
     # arguments
-    if len(sys.argv) < 6:
-        print('''Usage: python amtrakstatusnotifier.py <train number> <station> <delay> <admin email> <email 1> [email 2]''')
+    if len(sys.argv) < 5:
+        print('''Usage: python amtrakstatusnotifier.py <train number> <station> <admin email> <email 1> [email 2]''')
         sys.exit(0x2fe3)
     trainNumber = int(sys.argv[1])
     station = sys.argv[2]
-    delay = int(sys.argv[3])
-    adminAddress = sys.argv[4]
-    addresses = sys.argv[5:]
+    adminAddress = sys.argv[3]
+    addresses = sys.argv[4:]
     # Monitor
     try:
         monitor = StatusMonitor.StatusMonitor()
-        monitor.run(trainNumber, station, delay, addresses)
+        monitor.run(trainNumber, station, addresses)
     except Exception as error:
         message = 'Amtrak Status notifier raised an exception: {}'.format(error)
         # Save to file
